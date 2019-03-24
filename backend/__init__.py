fb6864c42e07524d468d2f5994bdff402d1b8967
Dante12129 <Blahduke31398ap>
2019-03-23 20:39:18 -0400

    Create API Blueprint
    Create the blueprint for the API and add it to the app.

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 
+from . import api
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Nothing to see here.'
+
+
+app.register_blueprint(api, url_prefix='/api')
---
+++ backend/api.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+api_views = Blueprint('api', __name__)
