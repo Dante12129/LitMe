0944cfc104eaa9f797177b0d638105dd29d84129
Dante12129 <dan9coasterdan@gmail.com>
2019-03-24 03:25:03 -0400

    Create arduino.ino

---
+++ frontend/arduino.ino
@@ -0,0 +1,22 @@
+int button_pin = A0;
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(button_pin, INPUT);
+}
+
+bool last_button = false;
+void loop()
+{
+    bool button = digitalRead(button_pin);
+    if (last_button != button)
+    {
+        if (button) {
+            Serial.println("Cool");
+        }
+        delay(100);
+    }
+    last_button = button;
+}
+© 2019 GitHub, Inc.
