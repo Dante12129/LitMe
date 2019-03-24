5cb62c238d2ff90d31ad8c3ab2a14741f6810b24
Dante12129 <Blahduke31398ap>
2019-03-23 21:55:47 -0400

    Implement Some API Endpoints
    These are post() and dinner().

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect, url_for
+import requests
 
-api_views = Blueprint('api', __name__)
+from .constants import BOT
+
+api = Blueprint('api', __name__)
+
+
+def sendToGroupMe(string):
+    r = requests.post('https://api.groupme.com/v3/bots/post', data={'bot_id': BOT, 'text': string})
+    return r.status_code
+
+
+@api.route('/post', methods=['POST'])
+def post():
+    data = request.get_json(force=True)
+    if data is not None:
+        message = data.get('message')
+        return '{}', sendToGroupMe(message)
+    return '{}', 200
+
+
+@api.route('/dinner', methods=['POST'])
+def dinner():
+    sendToGroupMe('Someone wants to go to dinner.')
+    return '[]', 200
