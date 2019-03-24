e791b6d82724fac0a66ecb8357cf6b30e5c06aa7
Dante12129 <dan9coasterdan@gmail.com>
2019-03-24 03:24:15 -0400

    Create dragonboard.py

---
+++ frontend/dragonboard.py
@@ -0,0 +1,26 @@
+import serial
+import requests
+
+#use requests to send data to a url (want it to be a post)
+
+connected = False
+ser = serial.Serial(
+    port = '/dev/tty96B0', #/dev/ttyUSB0 on Linux
+    baudrate = 9600, #9600 on the DragonBoard
+    timeout = 3
+)
+if not ser.isOpen():
+    ser.open()
+if ser.isOpen():
+    print(ser.name + " is Open!")
+    connected = True
+
+#we want to send the request when the button is pushed but how do we know when the button is pushed
+#we cant have the button press in arduino activate the request because its the wrong program
+
+while connected == True:
+    if ser.inWaiting() > 0:
+        reading = ser.readline().decode('ascii')
+        print(reading, end='')
+        if reading[:4] == 'Cool':
+            request = requests.post('https://litme.net/api/dinner', timeout=1)
